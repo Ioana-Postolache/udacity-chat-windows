865d8d7fe649bf18de4f85551d036edd9eb895ee
Ioana-Postolache <ioana.postolache.zaharia@gmail.com>
2018-12-15 20:34:18 +0000

    add-functionality

---
+++ src/MessageList.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+class MessageList extends Component{
+  static propTypes={
+    user: PropTypes.object.isRequired,
+    messages: PropTypes.array.isRequired
+  };
+  render(){
+    const {user, messages}=this.props;
+    return(
+         <ul className="message-list">
+              {messages.map((message, index) => (
+                <li
+                  key={index}
+                  className={
+                    message.username === user.username ? 'message sender' : 'message recipient'
+                  }
+                >
+                  <p>{`${message.username}: ${message.text}`}</p>
+                </li>
+              ))}
+            </ul>
+)}}
+
+export default MessageList;
